4f50620bc9ea18fd039571c2522c352aefcca70e
Azusena Arroyo <azusenaarroyo@Azusenas-MBP.attlocal.net>
2021-12-04 17:51:37 -0600

    adding retirement info and queries.sql

--- schema.sql
+++ schema.sql
@@ -42,14 +42,16 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
  	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
-);
+  	PRIMARY KEY (title, emp_no, from_date)
+);	
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  	PRIMARY KEY (emp_no, dept_no)
 );
+
